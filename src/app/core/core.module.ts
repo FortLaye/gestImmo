090cda87c11fe17892417ec49addbee08977491e
Abdoulaye Mangane <abdoulaye.mangane2@orange-sonatel.com>
2024-03-11 09:45:59 +0000

    project initialize

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    SharedModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class CoreModule { }
